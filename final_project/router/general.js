a966057d0d6916a7519b3b42035d762bdb3ef7b8
Fahad Raza <fahadraza7060@gmail.com>
2026-08-29 17:51:02 +0500

    update the code

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -31,6 +31,52 @@ public_users.post("/register", (req, res) => {
     .json({ message: "Customer successfully registered. Now you can login." });
 });
 
+// Get the book list available in the shop
+public_users.get("/", function (req, res) {
+  return res.status(200).send(JSON.stringify(books, null, 2));
+});
+
+// ----- Get book details based on ISBN ------
+public_users.get("/isbn/:isbn", function (req, res) {
+  const isbn = req.params.isbn;
+
+  if (books[isbn]) {
+    return res.status(200).json(books[isbn]);
+  }
+
+  return res.status(404).json({ message: "Book not found" });
+});
+
+// ----- Get book details based on author -------
+public_users.get("/author/:author", function (req, res) {
+  const author = req.params.author;
+
+  const result = Object.values(books).filter(
+    (book) => book.author.toLowerCase() === author.toLowerCase(),
+  );
+
+  if (result.length > 0) {
+    return res.status(200).json(result);
+  }
+
+  return res.status(404).json({ message: "No books found for this author" });
+});
+
+// ----- Get all books based on title --------
+public_users.get("/title/:title", function (req, res) {
+  const title = req.params.title;
+
+  const result = Object.values(books).filter(
+    (book) => book.title.toLowerCase() === title.toLowerCase(),
+  );
+
+  if (result.length > 0) {
+    return res.status(200).json(result);
+  }
+
+  return res.status(404).json({ message: "No books found with this title" });
+});
+
 function getAllBooks() {
   axios
     .get("http://localhost:5000/")
