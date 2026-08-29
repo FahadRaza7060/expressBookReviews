d66d184b0572c398a451d31691ea454a53143ecb
Fahad Raza <fahadraza7060@gmail.com>
2026-08-29 12:19:58 +0500

    complete registration and login routes and also complete the authenticated book review routes for authenticated users

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -1,28 +1,72 @@
-const express = require('express');
-const jwt = require('jsonwebtoken');
+const express = require("express");
+const jwt = require("jsonwebtoken");
 let books = require("./booksdb.js");
 const regd_users = express.Router();
 
 let users = [];
 
-const isValid = (username)=>{ //returns boolean
-//write code to check is the username is valid
-}
+const isValid = (username) => {
+  // ----- write code to check is the username is valid ------
+  const user = users.find((user) => user.username === username);
+  return !user; // return true if user is present
+};
 
-const authenticatedUser = (username,password)=>{ //returns boolean
-//write code to check if username and password match the one we have in records.
-}
+const authenticatedUser = (username, password) => {
+  // -------- write code to check if username and password match the one we have in records. --------
+  const user = users.find(
+    (user) => user.username === username && user.password === password,
+  );
 
-//only registered users can login
-regd_users.post("/login", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  return !!user;
+};
+
+// only registered users can login
+regd_users.post("/login", (req, res) => {
+  // ----- 2nd: Write your code here -----
+  const { username, password } = req.body;
+
+  if (!username || !password) {
+    return res.status(400).json({
+      message: "Username and password are required",
+    });
+  }
+
+  if (!authenticatedUser(username, password)) {
+    return res.status(401).json({ message: "Invalid username or password" });
+  }
+
+  // Generate JWT
+  const accessToken = jwt.sign({ username: username }, "access", {
+    expiresIn: "1h",
+  });
+
+  // Save token in session
+  req.session.authorization = { accessToken };
+
+  return res.status(200).json({ message: "Customer successfully logged in" });
+
+  // return res.status(300).json({message: "Yet to be implemented"});
 });
 
 // Add a book review
 regd_users.put("/auth/review/:isbn", (req, res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const username = req.user.username;
+  const isbn = req.params.isbn;
+  const review = req.body.review;
+
+  if (!books[isbn]) {
+    return res.status(404).json({
+      message: "Book not found",
+    });
+  }
+
+  books[isbn].reviews[username] = review;
+
+  return res.status(200).json({
+    message: "Review added/updated successfully",
+    reviews: books[isbn].reviews,
+  });
+
 });
 
 module.exports.authenticated = regd_users;
--- final_project/router/general.js
+++ final_project/router/general.js
@@ -1,13 +1,35 @@
 const express = require('express');
 let books = require("./booksdb.js");
+
 let isValid = require("./auth_users.js").isValid;
 let users = require("./auth_users.js").users;
+
 const public_users = express.Router();
 
 
-public_users.post("/register", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+public_users.post("/register", (req, res) => {
+  // ----- Write your code here -------
+  const { username, password } = req.body;
+  
+  // Check if username and password are provided
+  if (!username || !password) {
+    return res.status(400).json({
+      message: "Username and password are required"
+    });
+  }
+
+  // Check if the username already exists
+  if (!isValid(username)) {
+    return res.status(409).json({ message: "Username already exists" });
+  }
+
+  // register the new user
+  users.push({ username, password});
+
+  return res.status(201).json({ message: "Customer successfully registered. Now you can login." });
+
+  // return res.status(300).json({message: "Yet to be implemented"});
+
 });
 
 // Get the book list available in the shop
